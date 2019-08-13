7728fe13a232bfcca114ba84ae8a6c0147ae7900
Student <example@example.com>
2019-08-13 09:57:12 -0700

    learn for simple effect for jQuery

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var add =function(25,25) {
+  return 25+25;
+
+};
+var number1 = parseInt(prompt("45"));
+var number2 = parseInt(prompt)("44"));
+var result = add(45,44);
+alert(result);
+
+jQuery ("h1").click(function() {
+  alert("here is Korea");
+});
+
+jQuery("p").click(function() {
+  alert("This is what people tell us")
+
+});
